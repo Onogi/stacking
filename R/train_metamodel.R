7ea6502d4054c5e33fbc7a57f30ec3ad988633e3
Onogi <onogiakio@gmail.com>
2024-09-30 12:51:54 +0900

    コメント付加しました（#=>の部分）

--- R/train_metamodel.R
+++ R/train_metamodel.R
@@ -1,4 +1,7 @@
 train_metamodel <- function(basemodel_train_result, which_to_use, Metamodel, cross_validation = FALSE, TrainEachFold = FALSE, Training_X_list = NULL){
+
+  #=>basemodel_train_result（train_basemodelの出力）の要素にTraining_X_listが含まれているので、引数にはいらないのでは？
+  #=>またcross_validationを、basemodel_train_resultの要素とすると、引数に入れる必要はなくなります。
   
   nb <- basemodel_train_result$no_base
   
@@ -57,26 +60,39 @@ train_metamodel <- function(basemodel_train_result, which_to_use, Metamodel, cro
         xsoeji <- matrix(0, nrow = ly %/% nfold + 1, ncol = nfold)
         xsoeji[1:ly] <- 1:ly
       }
+      #=>このxsoejiはcross_validation=TRUEのときに学習用サンプルを指定する機能をするものです。
+      #=>ランダムサンプリングのときは、別に考える必要があります。
+      
       metamodel <- as.list(numeric(nfold))
       for(fold in 1:nfold){
         test <- xsoeji[, fold]
         #Combine the test portion of valpr and the corresponding part of X.randomised vertically
         combined_data <- rbind(valpr[test, ], Training_X_list[[fold]])
+
+        #=>xsoejiから作るtestも、cross_validation=TRUEのときに機能するものです
+        
         metamodel[[fold]] <- train(combined_data,
                                    basemodel_train_result$Y.randomised[test],
                                    method = Metamodel)
+
+        #=>cross_validation = FALSEのときは、Xが強制的にmetamodelのtrainingに使われているようです。
+        #=>一方でcross_validation = TRUEのときは、Xはmetamodelのtrainingに使われていません。
+        #=>cross validationであってもなくても、Xをmetamodelのtrainingに加えるか否か、引数で指定する必要があるでしょう。
       }
     } else {
       #Combine all of valpr and the saved X.randomised vertically
       combined_X_randomised <- do.call(rbind, Training_X_list)
       combined_data <- rbind(valpr, combined_X_randomised)
       metamodel <- train(combined_data, basemodel_train_result$Y.randomised, method = Metamodel)
+
+      #=>ここも78から80行目と同じコメントです
     }
     
     metamodel_train_result <- list(train_result = metamodel,
                                    which_to_use = which_to_use,
                                    TrainEachFold = TrainEachFold)
     metamodel_train_result
+    #=>metamodel_train_resultが、cross_validation=TRUEのときにも出力されるようにしないといけません
   }
   }
 
