0328759af66ccfe68e8972b0ef3c5039a0585a2b
IshibashiT96 <168622539+IshibashiT96@users.noreply.github.com>
2024-10-31 12:56:34 +0900

    Update stacking_train.R

--- R/stacking_train.R
+++ R/stacking_train.R
@@ -1,10 +1,10 @@
-stacking_train <- function(X, Y, Nfold, Method, Metamodel, TrainEachFold = FALSE, core = 1){
+stacking_train <- function(X, Y, Nfold, Method, Metamodel, TrainEachFold = FALSE, core = 1, cross_validation = FALSE, use_X = FALSE, num_sample, proportion = 0.8) {
 
-  base <- train_basemodel(X, Y, Nfold, Method, core)
+  base <- train_basemodel(X, Y, Nfold, num_sample, Method, core, cross_validation, proportion)
 
   which_to_use <- 1:base$no_base
 
-  meta <- train_metamodel(base, which_to_use, Metamodel, TrainEachFold, cross_validation)
+  meta <- train_metamodel(base, which_to_use, Metamodel, TrainEachFold, use_X)
 
   stacking_train_result<-list(base = base,
                               meta = meta
