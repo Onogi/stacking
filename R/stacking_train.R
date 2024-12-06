8ce1d15c713362a5cd15097695472eed0209c273
IshibashiT96 <168622539+IshibashiT96@users.noreply.github.com>
2024-12-06 16:58:55 +0900

    Update stacking_train.R

--- R/stacking_train.R
+++ R/stacking_train.R
@@ -1,7 +1,5 @@
 stacking_train <- function(X, Y, Method, Metamodel, core = 1, cross_validation = FALSE, use_X = FALSE, TrainEachFold = FALSE, Nfold = 10, num_sample = 10, proportion = 0.8) {
-  ################################################################################
-  #train_basemodel同様にNfoldの位置を変更して初期値を与える。以下もNfold1の位置が要修正
-  ################################################################################
+
   base <- train_basemodel(X, Y, Method, core, cross_validation, Nfold, num_sample, proportion)
 
   which_to_use <- 1:base$no_base
